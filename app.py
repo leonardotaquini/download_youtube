be6e22b04f695682b02a557cf93f03af1915ca6d
Leonardo Taquini <leonardogtaquini@gmail.com>
2024-04-17 15:57:16 -0300

    app funcional

---
+++ app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.middleware.cors import CORSMiddleware
+from download import download_mp3
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.post("/download")
+async def download(req: Request):
+    try:
+        body = await req.json()
+        url = body["url"]
+        filename =  ( f"{ download_mp3( url ) }.mp3" ).replace(" ", "%20")
+        return { "url_download": f"http://localhost:8000/download/{ filename }" } 
+        
+    except Exception as e:
+        print(e)
+        return { "error": 'Error al descargar la musica.' }
+
+@app.get("/download/{filename}")
+async def download(req: Request):
+    filename = req.path_params.get("filename")
+    return FileResponse(f'./static/{filename}', filename = f'{filename}', media_type="audio/mp3")
---
+++ download.py
@@ -0,0 +1,22 @@
+import pytube
+def download_mp3(url):
+
+    try:
+        link = str(url)
+        yt = pytube.YouTube(link)
+        audio = yt.streams.filter(only_audio=True).first()
+
+        if audio is None:
+            print("Audio stream not found for this video. Downloading may not be possible.")
+            exit()
+
+        audio.download( filename=f"./static/{ audio.title }.mp3")
+        return audio.title
+          
+
+    except pytube.exceptions.PyTubeError as e:
+            print("An error occurred during download:", e)
+
+
+    except Exception as e: 
+        print("Unexpected error:", e)
